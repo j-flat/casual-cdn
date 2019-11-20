17bb8b60bd866bf6af284cd1b5eab2fd7c19b25e
Joni Kämppä <joni.kamppa@sc5.io>
2019-11-20 10:04:26 +0100

    update variables

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,6 @@
-variable "name" { default = "main-cdn-server" }
+variable "machine_name" { default = "main-cdn-server" }
 variable "machine_type" { default = "n1-standard-1" }
 variable "image" { default = "centos-cloud/centos-6" }
 variable "region" { default = "europe-north1" }
 variable "zone" { default = "europe-north1-a" }
+variable "boot_disk_size" { default = "20" }
