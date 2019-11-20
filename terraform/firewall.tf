aa895940b9f2bf8b2a6f33aa06dd4ae5b043b092
Joni Kämppä <joni.kamppa@sc5.io>
2019-11-20 10:09:49 +0100

    Add firewall rules

---
+++ terraform/firewall.tf
@@ -0,0 +1,35 @@
+resource "google_compute_firewall" "allow_http" {
+    name = "allow-http"
+    network = "default"
+
+    allow {
+        protocol "tcp"
+        ports = ["80", "8080"]
+    }
+
+    target_tags = ["allow-http"]
+}
+
+resource "google_compute_firewall" "allow_https" {
+    name = "allow-https"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports = ["443"]
+    }
+
+    target_tags = ["allow-https"]
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+    name = "allow-ssh"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports = ["22"]
+    }
+
+    target_tags = ["allow-ssh"]
+}
