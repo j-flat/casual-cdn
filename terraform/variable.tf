8e0a6e8d2ebebf4c91ee37729ad5acacbd89e91d
Joni Kämppä <joni.kamppa@sc5.io>
2019-11-22 10:09:40 +0100

    Change default machine_type to smaller

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,6 +1,6 @@
 variable "machine_name" { default = "main-cdn-server" }
-variable "machine_type" { default = "f1-micro" }
-variable "image" { default = "centos-cloud/centos-6" }
+variable "machine_type" { default = "g1-small" }
+variable "image" { default = "centos-cloud/centos-7" }
 variable "region" { default = "europe-north1" }
 variable "zone" { default = "europe-north1-a" }
 variable "boot_disk_size" { default = "20" }
